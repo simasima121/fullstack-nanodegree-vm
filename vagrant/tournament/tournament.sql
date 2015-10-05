434705aff1846a43481bcb2969a4c8364cbdb190
Sim Virdi <simeransvirdi@gmail.com>
2015-10-05 16:28:24 +0100

    Created Database and tables

--- vagrant/tournament/tournament.sql
+++ vagrant/tournament/tournament.sql
@@ -6,4 +6,31 @@
 -- You can write comments in this file by starting them with two dashes, like
 -- these lines here.
 
+/* what needs to be in the database?
+
+1) players - id (serial), name, who they've played (matches), their score (playerStandings), 
+who won/lost (reportMatch), who next opponent is (swissPairings)
+
+2) unique id for tournaments
+
+- when players add to tournament, database should create unique id for each player using serial
+- SQL aggregation when wanting to count or adding up
+- try using loop in database queries
+- if database calls get confusing use views
+
+will have player table with name and id
+have another table with player id, their wins, the matches they have played
+
+*/
+
+
+
+CREATE DATABASE tournament;
+\c tournament
+CREATE TABLE players( id SERIAL,
+											name TEXT);
+
+CREATE TABLE matches( id SERIAL,
+										wins INTEGER,
+										games_played INTEGER);
 
