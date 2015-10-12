e2dbbe0aaad8551b48c18cd778326654471d74a7
Sim Virdi <simeransvirdi@gmail.com>
2015-10-12 18:21:29 +0100

    created restaurant page and allowed delete/edit

--- vagrant/catalog/webserver.py
+++ vagrant/catalog/webserver.py
@@ -1,9 +1,41 @@
 from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
 import cgi
 
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from database_setup import Restaurant, Base, MenuItem
+
+engine = create_engine('sqlite:///restaurantmenu.db')
+Base.metadata.bind = engine
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
+
 class webserverHandler(BaseHTTPRequestHandler):
 	def do_GET(self):
 		try:
+			# restaurant lists
+			if self.path.endswith("/restaurants"):
+				restaurants = session.query(Restaurant).all()
+				self.send_response(200)
+				self.send_header('Content-type', 'text/html')
+				self.end_headers()
+
+				output = ""
+				output += "<html><body>"
+				for restaurant in restaurants:
+					output += restaurant.name
+					output += "</br>"
+					output += "<a href='#'>Delete</a>"
+					output += "</br>"
+					output += "<a href='#'>Edit</a>"
+					output += "</br></br>"
+
+				output += "</body></html>"
+
+				self.wfile.write(output)
+				#print output
+				return
+
 			if self.path.endswith("/hello"):
 				self.send_response(200)
 				self.send_header('Content-type', 'text/html')
@@ -34,7 +66,6 @@ class webserverHandler(BaseHTTPRequestHandler):
 				self.wfile.write(output)
 				print output
 				return
-
 
 		except IOError:
 			self.send_error(404, "File not found %s" % self.path)
