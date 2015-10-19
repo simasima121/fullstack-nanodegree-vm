7604ba20eaf2dcfe2578e5ceacede49cb4767e0d
Sim Virdi <simeransvirdi@gmail.com>
2015-10-19 16:45:15 +0100

    updated database to use JSON

--- vagrant/catalog/database_setup.py
+++ vagrant/catalog/database_setup.py
@@ -25,6 +25,18 @@ class MenuItem(Base):
     restaurant_id = Column(Integer, ForeignKey('restaurant.id'))
     restaurant = relationship(Restaurant)
 
+    @property
+    def serialize(self):
+        #returns object data in easily serializable format
+        return {
+            'name': self.name,
+            'description': self.description,
+            'id': self.id,
+            'price': self.price,
+            'course': self.course,
+        }
+    
+
 
 engine = create_engine('sqlite:///restaurantmenu.db')
 
